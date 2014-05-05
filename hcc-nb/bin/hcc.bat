294760389f5da8dbcff7df44b60887e708bf7ab7
Alessandro Falappa <alex.falappa@gmail.com>
2014-05-05 12:21:15 +0200

    Fixed jvm memory option in windows bt scripts

--- hcc-nb/bin/hcc.bat
+++ hcc-nb/bin/hcc.bat
@@ -1,2 +1,2 @@
 rem launch jvm with natives in windows folder
-java -Xm128m -Dsun.java2d.noddraw=true -Djava.library.path=lib\windows -jar hcc.jar
+java -Xms128m -Dsun.java2d.noddraw=true -Djava.library.path=lib\windows -jar hcc.jar
--- hcc-nb/bin/hcc64.bat
+++ hcc-nb/bin/hcc64.bat
@@ -1,2 +1,2 @@
 rem launch jvm with natives in windows64 folder
-java -Xm128m -Dsun.java2d.noddraw=true -Djava.library.path=lib\windows64 -jar hcc.jar
+java -Xms128m -Dsun.java2d.noddraw=true -Djava.library.path=lib\windows64 -jar hcc.jar
