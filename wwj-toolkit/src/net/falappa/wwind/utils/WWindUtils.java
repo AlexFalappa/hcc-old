14c0cbcd80c21de81391ed7b2d6617b7c1db41b8
afalappa <afalappa@mantis>
2014-10-14 11:31:07 +0200

    WWindUtis: cleanup and addition of a random multipolygon method

--- wwj-toolkit/src/net/falappa/wwind/utils/WWindUtils.java
+++ wwj-toolkit/src/net/falappa/wwind/utils/WWindUtils.java
@@ -11,12 +11,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import net.falappa.wwind.helpers.ExtVisibilityViewController;
+import net.falappa.wwind.layers.MultiPolygonShapesLayer;
 import net.falappa.wwind.layers.SurfShapesLayer;
 import net.falappa.wwind.widgets.WWindPanel;
 
 /**
  * WorldWind related utility methods.
- * <p>
+ *
  * @author Alessandro Falappa
  */
 public final class WWindUtils {
@@ -29,7 +30,7 @@ public final class WWindUtils {
 
     /**
      * Parses a string of Lat Lon coordinates in degrees and creates a list of <code>LatLon</code> objects.
-     * <p>
+     *
      * @param posList a GML posList coordinates string (lat lon pairs)
      * @return a List of LatLon objects
      */
@@ -49,7 +50,7 @@ public final class WWindUtils {
 
     /**
      * Converts a list of <code>LatLon</code> derived objects into a string of Lat Lon coordinates in degrees.
-     * <p>
+     *
      * @param latlonList a List of LatLon derived objects
      * @return a GML posList coordinates string (lat lon pairs)
      */
@@ -66,7 +67,7 @@ public final class WWindUtils {
      * Add some random polygons to the {@link SurfShapesLayer} of the given name.
      * <p>
      * The polygons will have an id generated from the given prefix and an appended counter. The polygons will be generated over Europe.
-     * <p>
+     *
      * @param wwp the WWindPanel holding the layers
      * @param lname the SurfShapesLayer to add to
      * @param idPrefix the shape id prefix
@@ -90,16 +91,16 @@ public final class WWindUtils {
     }
 
     /**
-     * Add some random rectangular polygons in the given lat lon range to the {@link SurfShapesLayer} of the given name.
+     * Add some random rectangular polygons in the given lat lon range to the given {@link SurfShapesLayer}.
      * <p>
      * The polygons will have an id generated from the given prefix and an appended counter. The rectangles will be lat lon axis oriented
      * and have the specified maximum height and width.
      * <p>
      * <b>Note:</b> it is assumed that minLat &lt; maxLat , that minLon &lt; maxLon , that maxWidth &lt; (maxLon-minLon) and that maxHeight
      * &lt; (maxLat-minLat).
-     * <p>
+     *
      * @param wwp the WWindPanel holding the layers
-     * @param lname the SurfShapesLayer to add to
+     * @param layer the MultiPolygonShapesLayer to add to
      * @param idPrefix the shape id prefix
      * @param minLat minimum latitude
      * @param maxLat maximum latitude
@@ -108,13 +109,59 @@ public final class WWindUtils {
      * @param maxWidth maximum width
      * @param maxHeight maximum height
      */
-    public static void randPolys(WWindPanel wwp, String lname, String idPrefix, double minLat, double maxLat, double minLon, double maxLon, double maxWidth, double maxHeight) {
+    public static void randMultipolys(WWindPanel wwp, MultiPolygonShapesLayer layer, String idPrefix, double minLat, double maxLat, double minLon, double maxLon, double maxWidth, double maxHeight) {
+        List<LatLon> positions;
+        List<List<LatLon>> polys;
+        final double deltaLon = maxLon - minLon;
+        final double deltaLat = maxLat - minLat;
+        final double minWidth = maxWidth * 0.2;
+        final double minHeight = maxHeight * 0.2;
+        for (int i = 0; i < 20; i++) {
+            polys = new ArrayList<>();
+            positions = new ArrayList<>();
+            double btmLat = minLat + Math.random() * deltaLat;
+            double btmLon = minLon + Math.random() * deltaLon;
+            double w = minWidth + Math.random() * maxWidth;
+            double h = minHeight + Math.random() * maxHeight;
+            positions.add(LatLon.fromDegrees(btmLat, btmLon));
+            positions.add(LatLon.fromDegrees(btmLat, btmLon + w));
+            positions.add(LatLon.fromDegrees(btmLat + h * 0.4, btmLon + w / 2));
+            polys.add(positions);
+            positions = new ArrayList<>();
+            positions.add(LatLon.fromDegrees(btmLat + h, btmLon));
+            positions.add(LatLon.fromDegrees(btmLat + h, btmLon + w));
+            positions.add(LatLon.fromDegrees(btmLat + h * 0.6, btmLon + w / 2));
+            polys.add(positions);
+            layer.addMultiPoly(String.format("%s%d", idPrefix, i), polys);
+        }
+        wwp.redraw();
+    }
+
+    /**
+     * Add some random triangular multipolygons in the given lat lon range to the given {@link MultiPolygonShapesLayer}.
+     * <p>
+     * The multipolygons will have an id generated from the given prefix and an appended counter. The triangles bases will be lat lon axis
+     * oriented and have the specified maximum height and width.
+     * <p>
+     * <b>Note:</b> it is assumed that minLat &lt; maxLat , that minLon &lt; maxLon , that maxWidth &lt; (maxLon-minLon) and that maxHeight
+     * &lt; (maxLat-minLat).
+     *
+     * @param wwp the WWindPanel holding the layers
+     * @param layer the SurfShapesLayer to add to
+     * @param idPrefix the shape id prefix
+     * @param minLat minimum latitude
+     * @param maxLat maximum latitude
+     * @param minLon minimum longitude
+     * @param maxLon maximum longitude
+     * @param maxWidth maximum width
+     * @param maxHeight maximum height
+     */
+    public static void randPolys(WWindPanel wwp, SurfShapesLayer layer, String idPrefix, double minLat, double maxLat, double minLon, double maxLon, double maxWidth, double maxHeight) {
         List<LatLon> positions;
         final double deltaLon = maxLon - minLon;
         final double deltaLat = maxLat - minLat;
         final double minWidth = maxWidth * 0.2;
         final double minHeight = maxHeight * 0.2;
-        SurfShapesLayer ssl = (SurfShapesLayer) wwp.getSurfShapeLayer(lname);
         for (int i = 0; i < 20; i++) {
             positions = new ArrayList<>();
             double btmLat = minLat + Math.random() * deltaLat;
@@ -125,7 +172,7 @@ public final class WWindUtils {
             positions.add(LatLon.fromDegrees(btmLat, btmLon + w));
             positions.add(LatLon.fromDegrees(btmLat + h, btmLon + w));
             positions.add(LatLon.fromDegrees(btmLat + h, btmLon));
-            ssl.addSurfPoly(positions, String.format("%s%d", idPrefix, i));
+            layer.addSurfPoly(positions, String.format("%s%d", idPrefix, i));
         }
         wwp.redraw();
     }
@@ -135,7 +182,7 @@ public final class WWindUtils {
      * <p>
      * <b>Note:</b> the algorithm employed in this method works mainly for 2D views, on the 3D globe it works progressively worse as the
      * sector approaches the poles. Use {@link #flyToObjects(gov.nasa.worldwind.WorldWindow, java.lang.Iterable)} for better results.
-     * <p>
+     *
      * @param wwd the WorldWindow used for calculations
      * @param sector the lat lon range sector to fly to
      */
@@ -164,7 +211,7 @@ public final class WWindUtils {
 
     /**
      * Animates the map, in a sort of fligth, to bring the given position into view.
-     * <p>
+     *
      * @param wwd the WorldWindow used for calculations
      * @param position the coordinates and elevation to fly to
      */
@@ -180,7 +227,7 @@ public final class WWindUtils {
 
     /**
      * Animates the map, in a sort of fligth, to bring the given objects into view.
-     * <p>
+     *
      * @param wwd the WorldWindow used for calculations
      * @param objs an array of WorldWind objects
      */
